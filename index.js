1f38d7cc2545022aacec37ff2ca98c393f3659a1
andreysek <andreysek@icloud.com>
2022-03-22 13:46:56 +0300

    Added files

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var acc = document.getElementsByClassName("title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
